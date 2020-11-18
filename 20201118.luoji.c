98da8418938f50bbf902d1c354fa9d3cf0c5ac26
hshshs168 <74294221+hshshs168@users.noreply.github.com>
2020-11-18 09:18:05 +0800

    Create 20201118.luoji.c

---
+++ 20201118.luoji.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+void main(){
+int a=3,b=4,c=5;
+int x,y;
+if(!(a+b)+c-1&&b+c/2)
+{
+	printf("%d\n",1);
+}
+else
+{
+	printf("%d\n",0);
+}
+if(a+b>c&&b==c)
+{
+	printf("d\n",1);
+}
+else
+{
+	printf("%d\n",0);
+}
+if(a||b+c&&b-c)
+{
+	printf("%d\n",1);
+}
+else
+{
+	printf("d\n",0);
+}
+if(!(a>b)&&!c||1)
+{
+	printf("%d\n",1);
+}
+else
+{
+	printf("&d\n",0);
+}
+if(!(x=a)&&(y=b)&&0)
+{
+	printf("%d\n",1);
+}
+else
+{
+	printf("%d\n",0);
+}
+}
