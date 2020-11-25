125cea6bf98e76f8cf8eb03126dc3d84c739ef3f
hshshs168 <74294221+hshshs168@users.noreply.github.com>
2020-11-25 09:52:30 +0800

    Create 112502.QS排名查询.app.c

---
+++ 112502.QS排名查询.app.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+void main(){
+int a;
+printf("please enter ranking:\n");
+scanf("%d",&a);
+if(a==1)
+{
+	printf("MIT");
+}
+else if(a==2)
+{
+	printf("Stanford University");
+}
+else if(a==3)
+{
+	printf("harvard university");
+}
+else if(a==4)
+{
+	printf("Oxford");
+}
+else if(a==5)
+{
+	printf("加州理工大学");
+}
+else if(a==6)
+{
+	printf("Federal Polytechnic University of Zurich");
+}
+else if(a==7)
+{
+	printf("剑桥大学");
+}
+else if(a==8)
+{
+	printf("伦敦大学学院");
+}
+else if(a==9)
+{
+	printf("帝国理工学院");
+}
+else if(a=10)
+{
+	printf("芝加哥大学");
+}
+else if(a>10)
+{
+	printf("老铁，请稍后再拨。");
+}
+}
