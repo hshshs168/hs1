f44dc5b390f330bf43fa3c8eb274b23af52bef99
hshshs168 <74294221+hshshs168@users.noreply.github.com>
2020-12-09 17:05:16 +0800

    Create 120905.串长度.app.c

---
+++ 120905.串长度.app.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    char a1[]="forgein";
+    char a2[]="外国人";
+
+    printf("%d",sizeof(a1));
+    printf("\n");
+    printf("%d",sizeof(a2));
+
+}
