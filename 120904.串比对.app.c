d24b65d510336c87220b99c97e0c6f83468e91e6
hshshs168 <74294221+hshshs168@users.noreply.github.com>
2020-12-09 16:54:45 +0800

    Create 120904.串比对.app.c

---
+++ 120904.串比对.app.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    char a1[]="山西";
+    char a2[]="广西";
+    if(!strcmp(a1,a2))
+    {
+
+        printf("相同");
+        }
+    else
+    {
+        printf("不相同");
+    }
+
+
+}
